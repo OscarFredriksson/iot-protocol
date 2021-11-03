27c014e2e83ea34b3d5c63dbdafd51ec5ca52800
Oscar Fredriksson <osfr1701@student.miun.se>
2021-11-03 17:15:06 +0100

    basic GET request is formatted

--- project_0/main.cpp
+++ project_0/main.cpp
@@ -6,22 +6,9 @@ int main()
 {
     const int port = 8080;
 
-    HttpConnection http(port);
+    HttpServer server(port);
 
-    while(true){ 
-        if(!http.connect()){
-            std::cout << "failed to connect\n";
-            return 1;
-        }
-
-        HttpConnection::Request msg = http.receive();
-        std::cout << msg.type << " " << msg.url << "\n";
-
-        const std::string html = "<body><h1>Hello World!</h1></body>";
-
-        http.respond(HttpConnection::OK, html);
-        http.close();
-    }
+    
 
     return 0;
 }
