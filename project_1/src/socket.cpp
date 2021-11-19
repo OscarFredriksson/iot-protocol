a2ece2b98870c7348bf38625d047789a0423fffd
Oscar Fredriksson <osfr1701@student.miun.se>
2021-11-19 16:34:16 +0100

    code cleanup

--- project_1/src/socket.cpp
+++ project_1/src/socket.cpp
@@ -11,28 +11,20 @@ int Socket::connect()
 {
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     
-    // Creating socket file descriptor 
     if (sockfd == 0) { 
         std::cerr << "socket failed\n"; 
         return -1; 
     } 
- 
-    hostent* server = gethostbyname(hostname.c_str());
-    
-    if(server == NULL) {
-        std::cerr << "ERROR, no such host\n";
-        return -1;
-    }
 
     sockaddr_in addr_in;
     addr_in.sin_family = AF_INET;
     addr_in.sin_port = htons(port);
 
-    addrinfo* first_addr = NULL;
+    addrinfo* first_addr = nullptr;
     
     getaddrinfo(hostname.c_str(), std::to_string(port).c_str(), 0, &first_addr);
 
-    for (addrinfo* addr_it = first_addr; addr_it; addr_it = addr_it->ai_next) {
+    for (addrinfo* addr_it = first_addr; addr_it != nullptr; addr_it = addr_it->ai_next) {
         
         if (::connect(sockfd, addr_it->ai_addr, sizeof(addr_in)) < 0) { 
             std::cerr << "ERROR connecting\n";
@@ -49,8 +41,10 @@ int Socket::connect()
 std::vector<char> Socket::receive()
 {
     int msgSize = 1024;
-    char msg[msgSize]; 
+    char msg[msgSize];
     int byteCount = read(sockfd, msg, msgSize);
+
+    std::cout << "Bytes: " << byteCount << "\n";
 
     return std::vector<char>(msg, msg + byteCount);
 }
