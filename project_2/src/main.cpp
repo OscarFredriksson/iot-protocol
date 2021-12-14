fbeb066163451344e6646c6c6a7ee982d269e08a
Oscar Fredriksson <osfr1701@student.miun.se>
2021-12-14 01:58:37 +0100

    cleaned main

--- project_2/src/main.cpp
+++ project_2/src/main.cpp
@@ -1,15 +1,8 @@
-#include <fstream>
-#include <string>
-#include <vector>
-
 #include "broker.h"
 
 int main() {
   const int port = 1883;
-
   mqtt::Broker broker(port);
-
-  broker.start();
-
+  roker.start();
   return 0;
 }
